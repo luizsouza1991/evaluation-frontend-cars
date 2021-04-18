072e43bceb29d43d40ae5bf07e3efa4b606368d7
luiz <luizsouza1945@gmail.com>
2021-04-18 11:52:10 -0400

    Adicionando dados de carros por década, marca, status de venda e registros semanais

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -2,5 +2,22 @@ import { environment } from '../../environments/environment';
 
 export const Constant = {
     BASE_URL: environment.path,
-    CAR: 'veiculos/'
+    CAR: 'veiculos/',
+    MARCAS: [
+        {
+            'nome': 'Honda'
+        },
+        {
+            'nome': 'Volkswagen'
+        },
+        {
+            'nome': 'Chevrolet'
+        },
+        {
+            'nome': 'Fiat'
+        },
+        {
+            'nome': 'Ford'
+        }
+    ]
 }
--- src/app/pages/car/car.component.ts
+++ src/app/pages/car/car.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Constant } from 'src/app/constants/constants';
 import { Car } from 'src/app/models/car.model';
 import { CarService } from 'src/app/services/car/car.service';
 import { CarViewComponent } from './car-view/car-view.component';
@@ -13,6 +14,13 @@ export class CarComponent implements OnInit {
 
   public cars: Car[] = [];
   public search: string = null;
+  public carRegisterLastWeek: number = 0;
+  public carForDecade: number = 0;
+  public unsoldCars: number = 0;
+  public marcas: any = Constant.MARCAS;
+  public carsForBrand: any = [];
+  public carsForDecade: any = [];
+  public carsForFormat: any;
 
   @ViewChild(FormCarComponent, { static: false })
   formCar: FormCarComponent;
@@ -25,11 +33,45 @@ export class CarComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.initCarsForBrand();
     this.getCars();
   }
 
+  public initCarsForBrand() {
+    this.marcas.map((marca: any) => {
+      this.carsForBrand[marca.nome] = 0;
+    });
+  }
+
   public async getCars() {
+    let currentDate = new Date();
+    let lastWeekDate = currentDate.setDate(currentDate.getDate() - 7);
+
     await this.carService.index().subscribe((data: Car[]) => {
+      data.map((car: Car) => {
+        let decadeCar = parseInt(car.ano.toString().substr(0,3)+0);
+        if (!car.vendido) {
+          this.unsoldCars++;
+        }
+
+        if (new Date(car.created_at) >= new Date(lastWeekDate) && new Date(car.created_at) <= new Date()) {
+          this.carRegisterLastWeek++;
+        }
+
+        this.marcas.map((marca: any) => {
+          if (marca.nome == car.marca) {
+            this.carsForBrand[marca.nome]++;
+          }
+        })
+
+        if (car.ano >= decadeCar && car.ano <= (decadeCar + 9)) {
+          if(isNaN(this.carsForDecade[decadeCar])) {
+            this.carsForDecade[decadeCar] = 0;
+          }
+          this.carsForDecade[decadeCar] = this.carsForDecade[decadeCar] + 1;
+        }
+      })
+      this.carsForDecadeFormat();
       this.cars = data;
     })
   }
@@ -39,6 +81,13 @@ export class CarComponent implements OnInit {
   }
 
   public updateList(car: Car) {
+    let decadeCar = parseInt(car.ano.toString().substr(0,3)+0);
+    this.carRegisterLastWeek++;
+    this.unsoldCars++;
+    this.carsForBrand[car.marca]++;
+    this.carsForDecade[decadeCar]++;
+    this.carsForDecadeFormat();
+
     this.cars = [...this.cars,car];
   }
 
@@ -55,6 +104,23 @@ export class CarComponent implements OnInit {
   }
 
   public removeFromList(car: Car) {
+    let currentDate = new Date();
+    let lastWeekDate = currentDate.setDate(currentDate.getDate() - 7);
+    let decadeCar = parseInt(car.ano.toString().substr(0,3)+0);
+
+    if (new Date(car.created_at) >= new Date(lastWeekDate) && new Date(car.created_at) <= new Date()) {
+      this.carRegisterLastWeek--;
+    }
+
+    if (!car.vendido) {
+      this.unsoldCars--;
+    }
+    
+    this.carsForBrand[car.marca]--;
+    this.carsForDecade[decadeCar] = this.carsForDecade[decadeCar] - 1;
+    
+    this.carsForDecadeFormat();
+
     this.cars = this.cars.filter((c: Car) => c.uuid != car.uuid);
   }
 
@@ -82,5 +148,12 @@ export class CarComponent implements OnInit {
     } else {
       this.getCars();
     }
+  }
+
+  public carsForDecadeFormat() {
+    this.carsForFormat = [];
+    this.carsForDecade.map((data, index) => {
+      this.carsForFormat = [...this.carsForFormat, {'decada': index, 'qtd_cars': data}]
+    })
   }
 }
